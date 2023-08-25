053a1e4f1ed9cac1b0743078036be1b23d7ec97b
sanyashetty <shettysanya@gmail.com>
2023-08-25 11:43:31 -0400

    setting up backend server

--- server/index.mjs
+++ server/index.mjs
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+import exerciseRoutes from "./routes/exerciseRoutes.mjs";
+import userRoutes from "./routes/userRoutes.mjs";
+import workoutRoutes from "./routes/workoutRoutes.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -9,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/record", records);
+app.use("/exercises", exerciseRoutes);
+app.use("/users", userRoutes);
+app.use("/workouts", workoutRoutes);
 
 // start the Express server
 app.listen(PORT, () => {
